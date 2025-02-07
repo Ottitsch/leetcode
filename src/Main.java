466c98a1764d38c9cd372706e04d5a8b357b5712
Franz Ottitsch <ottitsch.work@gmail.com>
2025-02-07 23:35:49 +0100

    reverseString

--- src/Main.java
+++ src/Main.java
@@ -306,6 +306,14 @@ public class Main {
             array[writeInd++] = rightPart[p2++];
         }
     }
+
+    public void reverseString(char[] s) {
+        for (int i = 0, j = s.length - 1; i < s.length / 2; i++, j--) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
 }
 
 /*
