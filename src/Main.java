d62e5e9f628cc7a912527dd5c402b6228b52c003
Franz Ottitsch <ottitsch.work@gmail.com>
2025-02-09 19:01:24 +0100

    Remove Duplicates from Sorted Array

--- src/Main.java
+++ src/Main.java
@@ -354,6 +354,16 @@ public class Main {
         Arrays.sort(nums1);
     }
 
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
 }
 
 /*
