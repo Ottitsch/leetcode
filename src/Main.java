5a79b32d64d2249887a412360344bb2a55bca296
Franz Ottitsch <ottitsch.work@gmail.com>
2025-02-09 18:29:16 +0100

    mergeAlternately

--- src/Main.java
+++ src/Main.java
@@ -332,6 +332,19 @@ public class Main {
         }
         return true;
     }
+
+    public String mergeAlternately(String word1, String word2) {
+        String str = "";
+        for(int i=0;i<word1.length()||i<word2.length();i++){
+            if(i<word1.length()){
+                str+=word1.toCharArray()[i];
+            }
+            if(i<word2.length()){
+                str+=word2.toCharArray()[i];
+            }
+        }
+        return str;
+    }
 }
 
 /*
