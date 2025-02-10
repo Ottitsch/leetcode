4bf85b324cc531589d0548aae83fca53e414c7c3
Franz Ottitsch <ottitsch.work@gmail.com>
2025-02-10 16:17:36 +0100

    Best Time to Buy and Sell Stock

--- src/Main.java
+++ src/Main.java
@@ -364,6 +364,20 @@ public class Main {
         }
         return j;
     }
+
+    public int maxProfit(int[] prices) {
+        int low=prices[0];
+        int profit=0;
+
+        for(int i=1;i<prices.length;i++){
+            if(low>prices[i]){
+                low=prices[i];
+            } else if(profit<prices[i]-low){
+                profit=prices[i]-low;
+            }
+        }
+        return profit;
+    }
 }
 
 /*
