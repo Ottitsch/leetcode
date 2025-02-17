8b224d537ba86219b81a85f28e994f94a867a6b8
ottitsch <e12024717@student.tuwien.ac.at>
2025-02-17 01:02:38 +0100

    Make The String Great

--- src/Main.java
+++ src/Main.java
@@ -540,6 +540,25 @@ public class Main {
         }
         return removeRecursive(sb.toString());
     }
+
+    public String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(c - stack.peek()) == 32) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!stack.isEmpty()) {
+            result.insert(0, stack.pop());
+        }
+
+        return result.toString();
+    }
 }
 
 /*
