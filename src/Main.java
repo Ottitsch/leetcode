c84e92decd20f54c0814d5081c27f03f99bf64c1
ottitsch <e12024717@student.tuwien.ac.at>
2025-02-19 20:07:39 +0100

    Binary Search

--- src/Main.java
+++ src/Main.java
@@ -657,6 +657,25 @@ public class Main {
 
         return opening.isEmpty();
     }
+
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(target < nums[mid]){
+                end = mid - 1;
+            } else if(target > nums[mid]){
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
 }
 
 /*
