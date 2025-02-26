38e64cf108b01c0afe1cdb42fbfbe9222e866bd2
Franz Ottitsch <ottitsch.work@gmail.com>
2025-02-26 22:19:21 +0100

    Length of Last Word

--- src/Main.java
+++ src/Main.java
@@ -746,6 +746,19 @@ public class Main {
         }
         return nums[nums.length/2];
     }
+
+    public int lengthOfLastWord(String s) {
+        int count = 0;
+        for(int i = s.length() - 1; i >= 0; i--) {
+            if(s.charAt(i) != ' ') {
+                count++;
+            }
+            else if(count > 0) {
+                break;
+            }
+        }
+        return count;
+    }
 }
 
 /*
