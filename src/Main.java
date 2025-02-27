20a606f99435b2b6ea2cab57761d93e7a4f3f9fe
ottitsch <e12024717@student.tuwien.ac.at>
2025-02-27 18:59:17 +0100

    Min Cost Climbing Stairs

--- src/Main.java
+++ src/Main.java
@@ -777,6 +777,19 @@ public class Main {
         }
         return hm.get(i);
     }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int first = cost[0];
+        int second = cost[1];
+        if (n<=2) return Math.min(first, second);
+        for (int i=2; i<n; i++) {
+            int curr = cost[i] + Math.min(first, second);
+            first = second;
+            second = curr;
+        }
+        return Math.min(first, second);
+    }
 }
 
 /*
