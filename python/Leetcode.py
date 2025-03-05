f572a94fad9e9d82c54e78898553b50ebc48f27c
ottitsch <e12024717@student.tuwien.ac.at>
2025-03-05 17:41:33 +0100

    Concatenation of Array

--- python/Leetcode.py
+++ python/Leetcode.py
@@ -17,3 +17,13 @@ def fizzBuzz(self, n):
             answer.append(str(i))
         i+=1
     return answer
+
+def getConcatenation(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[int]
+    """
+    ans = nums
+    for i in range(0,len(nums)):
+        ans.append(nums[i])
+    return ans
