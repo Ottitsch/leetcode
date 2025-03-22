8dbfec8fb204e11a21a11d7fae00a21624261732
Franz Ottitsch <ottitsch.work@gmail.com>
2025-03-22 14:28:33 +0100

    containsDuplicate

--- python/PracticeV1.py
+++ python/PracticeV1.py
@@ -1,2 +1,12 @@
 def getConcatenation(self, nums: list[int]) -> list[int]:
     return  nums + nums
+
+def containsDuplicate(nums: list[int]) -> bool:
+    mySet = set()
+    for num in nums:
+        if num in nums:
+            return True
+        mySet.add(num)
+    return False
+
+print(containsDuplicate([1,2,3,1]))
