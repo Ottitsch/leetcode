615ae5d116bb4881b84abae8836c44984d8e0981
ottitsch <e12024717@student.tuwien.ac.at>
2025-03-22 15:49:48 +0100

    twosum

--- python/PracticeV1.py
+++ python/PracticeV1.py
@@ -1,7 +1,7 @@
 from collections import Counter
 
 def getConcatenation(self, nums: list[int]) -> list[int]:
-    return  nums + nums
+    return nums + nums
 
 def containsDuplicate(nums: list[int]) -> bool:
     mySet = set()
@@ -14,4 +14,9 @@ def containsDuplicate(nums: list[int]) -> bool:
 def isAnagram(s: str, t: str) -> bool:
     return Counter(s) == Counter(t)
 
-print(isAnagram("he2y","yeh2"))
+def twoSum(nums: list[int], target: int) -> list[int]:
+    hM = {num: i for i, num in enumerate(nums)}
+    for i in range(len(nums)):
+        value = target-nums[i]
+        if value in hM and hM[value]!=i:
+            return [hM.get(target-nums[i]),i]
