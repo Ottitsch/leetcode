f5b2e19d4e49363eb16c387e9bb4845b62ac1c30
ottitsch <e12024717@student.tuwien.ac.at>
2025-03-24 02:40:32 +0100

    Remove Element

--- python/PracticeV1.py
+++ python/PracticeV1.py
@@ -29,3 +29,11 @@ def longestCommonPrefix(strs: list[str]) -> str:
         else:
             return res
     return res
+
+def removeElement(nums: list[int], val: int) -> int:
+    index = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[index] = nums[i]
+            index += 1
+    return index
