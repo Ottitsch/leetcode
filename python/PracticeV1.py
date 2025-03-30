3b4928da6d9ff0b147d651fc73942d9804830c4d
Franz Ottitsch <ottitsch.work@gmail.com>
2025-03-30 19:39:50 +0200

    Valid Parentheses

--- python/PracticeV1.py
+++ python/PracticeV1.py
@@ -117,6 +117,19 @@ def calPoints(operations: list[str]) -> int:
 
     return sum(resultStack)
 
+def isValid(s: str) -> bool:
+    d = {'(':')', '{':'}','[':']'}
+    stack = []
+    for i in s:
+        if i in d:  # 1
+            stack.append(i)
+        elif len(stack) == 0 or d[stack.pop()] != i:  # 2
+            return False
+    return len(stack) == 0
+
+
+
+
 
 
 
