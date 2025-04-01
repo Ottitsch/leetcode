11bf66ab1eb46edc721a86516dcdd92b471758ba
Franz Ottitsch <ottitsch.work@gmail.com>
2025-04-01 19:22:10 +0200

    Binary Search - corrected version

--- python/PracticeV1.py
+++ python/PracticeV1.py
@@ -166,13 +166,13 @@ def carFleet(target: int, position: list[int], speed: list[int]) -> int:
 
 def search(nums: list[int], target: int) -> int:
     left = 0
-    right = len(nums)
-    while left < right:
+    right = len(nums)-1
+    while left <= right:
         mid = (right + left) // 2
         if nums[mid] == target:
             return mid
         if nums[mid] < target:
-            left = mid
+            left = mid+1
         else:
-            right = mid
+            right = mid-1
     return -1
