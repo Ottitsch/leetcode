bf42842b0ec34e03558c9db5fece254d6f920c41
Franz Ottitsch <ottitsch.work@gmail.com>
2025-04-09 18:05:23 +0200

    Search in Rotated Sorted Array

--- python/PracticeV1.py
+++ python/PracticeV1.py
@@ -312,5 +312,20 @@ def findMin(nums: list[int]) -> int:
             left=mid+1
     return nums[left]
 
-print(findMin([5,6,7,8,9,1,2,3,4]))
-
+def search(nums: list[int], target: int) -> int:
+    left, right = 0, len(nums)-1
+    while left<=right:
+        mid = left+(right-left)//2
+        if nums[mid] == target:
+            return mid
+        if nums[mid]<nums[right]:
+            if nums[mid]<target<=nums[right]:
+                left=mid+1
+            else:
+                right=mid-1
+        else:
+            if nums[left]<=target<nums[mid]:
+                right=mid-1
+            else:
+                left = mid+1
+    return -1
