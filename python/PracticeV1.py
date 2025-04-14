0d4faa6787ed48636468eca837382237cdba6ded
Franz Ottitsch <ottitsch.work@gmail.com>
2025-04-14 13:30:55 +0200

    Jump Game II

--- python/PracticeV1.py
+++ python/PracticeV1.py
@@ -388,7 +388,19 @@ def lemonadeChange(bills):
             return False
     return True
 
+def jump(self, nums: list[int]) -> int:
+    n = len(nums)
+    jumps = 0
+    current_end = 0
+    farthest = 0
 
+    for i in range(n - 1):
+        farthest = max(farthest, i + nums[i])
+        if i == current_end:
+            jumps += 1
+            current_end = farthest
+
+    return jumps
 
 
 
