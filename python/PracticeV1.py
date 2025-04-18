21a61aeb48d60f66f074e32fbefa6b9b04e9341f
Ottitsch <ottitsch.work@gmail.com>
2025-04-18 14:42:21 +0200

    Reverse Bits

--- python/PracticeV1.py
+++ python/PracticeV1.py
@@ -411,6 +411,11 @@ def addBinary(a: str, b: str) -> str:
     return bin(x+y)[2:]
 
 
+def reverseBits(self, n: int) -> int:
+    n = bin(n)[2:].zfill(32)
+    rev = n[::-1]
+    return int(rev, 2)
+
 
 
 
