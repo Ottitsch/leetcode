906dd1f3d0b645c890217d3fe1348090b9d31138
Ottitsch <ottitsch.work@gmail.com>
2025-04-20 14:31:49 +0200

    Reverse Integer

--- python/PracticeV1.py
+++ python/PracticeV1.py
@@ -422,6 +422,19 @@ def missingNumber(self, nums: List[int]) -> int:
     return int(eSum-sum(nums))
 
 
+def reverse(self, x: int) -> int:
+    res = 0
+    if x < 0:
+        res = int(str(x)[1:][::-1]) * -1
+    else:
+        res = int(str(x)[::-1])
+        
+    if res > 2 ** 31 - 1 or res < -2 ** 31:
+        return 0
+        
+    return res
+
+
 
 
 
