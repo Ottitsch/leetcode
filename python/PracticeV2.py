41735055296e97c5dc6481d85f465a0837ce1495
Franz Ottitsch <ottitsch.work@gmail.com>
2025-04-25 17:40:27 +0200

    Two Sum

--- python/PracticeV2.py
+++ python/PracticeV2.py
@@ -12,3 +12,10 @@ def containsDuplicate(self, nums: List[int]) -> bool:
 
 def isAnagram(self, s: str, t: str) -> bool:
     return sorted(s) == sorted(t)
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+    hM = {num: i for i, num in enumerate(nums)}
+    for i in range(len(nums)):
+        value = target-nums[i]
+        if value in hM and hM[value]!=i:
+            return [hM.get(target-nums[i]),i]
