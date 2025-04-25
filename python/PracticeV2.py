2ace2f063d71e336577a4e35337dafc4548068bc
Franz Ottitsch <ottitsch.work@gmail.com>
2025-04-26 01:06:21 +0200

    Longest Common Prefix

--- python/PracticeV2.py
+++ python/PracticeV2.py
@@ -19,3 +19,13 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
         value = target-nums[i]
         if value in hM and hM[value]!=i:
             return [hM.get(target-nums[i]),i]
+
+def longestCommonPrefix(self, strs):
+    prefix = strs[0]
+    for word in strs[1:]:
+        while not word.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+
+    return prefix
