bac9afca3bb1aeedec7757291c2ff89b9caa03ea
Ottitsch <ottitsch.work@gmail.com>
2025-04-28 15:24:51 +0200

    Reverse String

--- python/PracticeV2.py
+++ python/PracticeV2.py
@@ -48,5 +48,9 @@ def removeRekursive(s: str,c: str,b: str) -> str:
         result+=char
     return result
 
-
-print(minRemoveToMakeValid("test(()"))
+def reverseString(s: List[str]) -> None:
+    left, right = 0, len(s) - 1
+    while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
