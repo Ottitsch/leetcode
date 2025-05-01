949690fce3e00e95c3d8a6f5298e8f18a5983858
Franz Ottitsch <ottitsch.work@gmail.com>
2025-05-01 02:17:28 +0200

    Get Students with Names and Top Notes

--- python/edabit.py
+++ python/edabit.py
@@ -14,4 +14,10 @@ def str_to_int(num):
 def total_overs(balls):
 	return float(int(balls/6)) + balls%6/10
 
-print(total_overs(945))
+def top_note(student):
+    return {
+        "name": student["name"],
+        "top_note": max(student["notes"])
+    }
+
+print(top_note({ "name": "John", "notes": [3, 5, 4] }))
