c568275676f80ac6d82baa384b64adc25397ff05
Franz Ottitsch <ottitsch.work@gmail.com>
2025-05-01 03:31:25 +0200

    Climbing Competition

--- python/edabit.py
+++ python/edabit.py
@@ -1,3 +1,4 @@
+import math
 from datetime import datetime
 
 
@@ -66,4 +67,13 @@ def encrypt(word):
             response+=char
     return response+"aca"
 
-print(encrypt("apple"))
+def climb(stamina, obstacles):
+    count = 0
+    for i in (a - b for a, b in zip(obstacles, obstacles[1:])):
+        mod = (i < 0) + 1
+        stamina -= -(-abs(i)//1) * mod
+        if stamina < 0: return count
+        count += 1
+    return count
+
+print(climb(5, [5, 4.2, 3, 3.5, 6, 4, 6, 8, 1]))
