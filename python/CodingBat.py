4808cc95e784a874e60c701cd30d9c7fb64ae682
ottitsch <e12024717@student.tuwien.ac.at>
2025-05-02 03:28:38 +0200

    count_evens

--- python/CodingBat.py
+++ python/CodingBat.py
@@ -32,4 +32,11 @@ def has22(nums):
             prev=0
     return False
 
+def count_evens(nums):
+    count=0
+    for num in nums:
+        if num%2==0:
+            count+=1
+    return count
+
 
