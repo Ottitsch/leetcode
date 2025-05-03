acb14b62dac51826c6f474c5f83fc594c0739487
Franz Ottitsch <ottitsch.work@gmail.com>
2025-05-03 20:21:22 +0200

    Python In-place Assignment

--- python/GetCracked.py
+++ python/GetCracked.py
@@ -1,21 +1,30 @@
 class SmallNumbers:
-    a = 1
-    b = 1
-    if (a is b):
-        print("A")
-    else:
-        print("B")
+    def run(self):
+        a = 1
+        b = 1
+        if (a is b):
+            print("A")
+        else:
+            print("B")
 
-    a = 257
-    b = 257
-    if (a is b):
-        print("C")
-    else:
-        print("D")
+        a = 257
+        b = 257
+        if (a is b):
+            print("C")
+        else:
+            print("D")
 
-    a = int("-6")
-    b = -6
-    if (a is b):
-        print("E")
-    else:
-        print("F")
+        a = int("-6")
+        b = -6
+        if (a is b):
+            print("E")
+        else:
+            print("F")
+
+class InPlaceAssignment:
+    def run(self):
+        t = (1, 2, [30, 40])
+        t[2] += [50, 60]
+
+if __name__ == "__main__":
+    SmallNumbers().run()
