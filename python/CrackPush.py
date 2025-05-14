4e335ea32643926e42a8d02db528f7435d9c33ed
ottitsch <e12024717@student.tuwien.ac.at>
2025-05-14 14:54:34 +0200

    A Number Walks Into a Base...

---
+++ python/CrackPush.py
@@ -0,0 +1,9 @@
+# A Number Walks Into a Base...
+print(0b10 + 0o10 + 0x10)
+"""
+Python allows us to write integers in different bases using specific prefixes: 0b for binary, 0o for octal, and 0x for hexadecimal. In the given code, we are adding three such numbers:
+0b10 is binary for 2
+0o10 is octal for 8
+0x10 is hexadecimal for 16
+Putting it all together, the expression becomes 2 + 8 + 16, which equals 26
+"""
