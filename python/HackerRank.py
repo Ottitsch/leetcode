ccff5661137fc481b8b16848307892cbc0f9508f
ottitsch <e12024717@student.tuwien.ac.at>
2025-05-16 20:11:01 +0200

    Minion Game

---
+++ python/HackerRank.py
@@ -0,0 +1,21 @@
+def minion_game(string):
+    vowels = "AEIOU"
+    
+    stuart_score = 0
+    kevin_score = 0
+    n = len(string)
+    
+    for i in range(n):
+        if string[i] in vowels:
+            kevin_score += n - i
+        else:
+            stuart_score += n - i
+            
+            
+    if stuart_score == kevin_score:
+        print("Draw")
+    else:
+        if stuart_score > kevin_score:
+            print("Stuart " + str(stuart_score))
+        else:
+            print("Kevin " + str(kevin_score))
