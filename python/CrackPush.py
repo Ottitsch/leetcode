82b5aa8afc3d7e1b063b11755d4ee7268badd8a2
ottitsch <e12024717@student.tuwien.ac.at>
2025-05-19 20:24:23 +0200

    A Slice of Confusion

--- python/CrackPush.py
+++ python/CrackPush.py
@@ -7,3 +7,15 @@ Python allows us to write integers in different bases using specific prefixes: 0
 0x10 is hexadecimal for 16
 Putting it all together, the expression becomes 2 + 8 + 16, which equals 26
 """
+
+# A Slice of Confusion
+text = "Python"[::-1][::-1]
+print(text)
+"""
+This might look like it's doing something fancy, but it’s just a double reversal.
+
+"Python"[::-1] reverses the string: "nohtyP"
+Then, [::-1] again reverses it back to the original: "Python"
+
+So the final result is just "Python"
+"""
