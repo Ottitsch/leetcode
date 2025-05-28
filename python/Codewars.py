1bdfc6455c33a69c31bc2b8b85cf4e502b2e72d0
Franz Ottitsch <ottitsch.work@gmail.com>
2025-05-29 00:58:46 +0200

    Solved [BUG] XCOM-409: Flight distance of Interceptor planes is miscalculated

--- python/Codewars.py
+++ python/Codewars.py
@@ -34,4 +34,18 @@ def opposite(number):
 
 print(opposite(1),opposite(14),opposite(-34))
 
+# speed of aircrafts is given in *knots*
+# travelTime is in *minutes*
+# travel distance should be returned in *kilometers*
+
+def travel_distance(avg_speed, travel_time):
+    KM_PER_MILE = 1.852
+    travel_hours = travel_time / 60
+    travel_miles = avg_speed * travel_hours
+    travel_kms = travel_miles * KM_PER_MILE
+    return travel_kms
+
+print(travel_distance(600, 60))
+print(travel_distance(800, 60))
+
 
