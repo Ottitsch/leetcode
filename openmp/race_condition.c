a02b87192e43c153877875db8de402614f86f566
Franz Ottitsch <ottitsch.work@gmail.com>
2025-06-11 20:08:26 +0200

    testing out race conditions in openmc

---
+++ openmp/race_condition.c
@@ -0,0 +1,25 @@
+// race_condition.c
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int sum = 0;
+
+    #pragma omp parallel for
+    for (int i = 0; i < 10000; i++) {
+        sum += i; // Race condition
+    }
+
+    printf("Sum: %d\n", sum);
+    sum = 0;
+
+    //no Race condition
+    #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < 10000; i++) {
+        sum += i;
+    }
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
+
