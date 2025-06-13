e987628ef21b9abfce7a2b802e2ebb0ad3f1537e
ottitsch <e12024717@student.tuwien.ac.at>
2025-06-13 20:16:03 +0200

    added timer to compare results

--- openmp/race_condition.c
+++ openmp/race_condition.c
@@ -1,35 +1,60 @@
-// race_condition.c
 #include <stdio.h>
 #include <omp.h>
 
+#define N 10000000
+#define TRIALS 10
+
 int main() {
-    int sum = 0;
+    double start, end;
+    int sum;
 
-    #pragma omp parallel for
-    for (int i = 0; i < 10000; i++) {
-        sum += i; // Race condition
+    // 1. Naive version (with race condition)
+    printf("== Naive version (Race Condition) ==\n");
+    for (int t = 0; t < TRIALS; t++) {
+        sum = 0;
+        start = omp_get_wtime();
+
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++) {
+            sum += i; // Race condition
+        }
+
+        end = omp_get_wtime();
+        printf("Trial %d: Sum = %d, Time = %f s\n", t+1, sum, end - start);
     }
 
-    printf("Sum: %d\n", sum);
-    sum = 0;
+    // 2. Reduction version (correct and efficient)
+    printf("\n== Reduction version ==\n");
+    for (int t = 0; t < TRIALS; t++) {
+        sum = 0;
+        start = omp_get_wtime();
 
-    //no Race condition
-    #pragma omp parallel for reduction(+:sum)
-    for (int i = 0; i < 10000; i++) {
-        sum += i;
-    }
-    printf("Sum: %d\n", sum);
-
-    //fixed with critical
-    #pragma omp parallel for
-    for (int i = 0; i < 10000; i++) {
-        #pragma omp critical
-        {
+        #pragma omp parallel for reduction(+:sum)
+        for (int i = 0; i < N; i++) {
             sum += i;
         }
+
+        end = omp_get_wtime();
+        printf("Trial %d: Sum = %d, Time = %f s\n", t+1, sum, end - start);
     }
 
-    printf("Sum: %d\n", sum);
+    // 3. Critical version (correct but slower)
+    printf("\n== Critical section version ==\n");
+    for (int t = 0; t < TRIALS; t++) {
+        sum = 0;
+        start = omp_get_wtime();
+
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++) {
+            #pragma omp critical
+            {
+                sum += i;
+            }
+        }
+
+        end = omp_get_wtime();
+        printf("Trial %d: Sum = %d, Time = %f s\n", t+1, sum, end - start);
+    }
 
     return 0;
 }
