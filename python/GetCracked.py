b110f88ae3ab8d34d7553f767ee1eab523300294
ottitsch <e12024717@student.tuwien.ac.at>
2025-06-18 15:37:59 +0200

    arrrrg

--- python/GetCracked.py
+++ python/GetCracked.py
@@ -49,20 +49,20 @@ class DisMe:
     def run(self):
         DisMe.dis.dis(self.myFunction)
         print(self.myFunction(5))
-
+"""
 class SimpleMath:
     from math import fma
 
     def run(self):
         print(int(self.fma(2, 2, 1_10_0)))
-
+"""
 class ZipIt:
     def run(self):
         arr1 = [1,2]
         arr2 = [1,2,3,4]
         arr3 = list(zip(arr1,arr2))
         print(len(arr3))
-
+"""
 class WhatAreWeReplacing:
     from copy import replace
     from collections import namedtuple
@@ -72,6 +72,7 @@ class WhatAreWeReplacing:
         t1 = MyTuple(1,2)
         t2 = WhatAreWeReplacing.replace(t1, x = 3)
         print(t1, t2)
+"""
 
 class MinMinister:
     def run(self):
@@ -93,13 +94,31 @@ class MyNameIs():
 
         print(f"{a.getNum()}{len(a.__dict__)}{a.__num}")
 
-
 class SoHowBigIsX():
     import sys
     def run(self):
         x = 5
         print(self.sys.getsizeof(x))
 
+class arrrg():
+    def run(self):
+        try:
+            print(self.foo(1,2,3))
+        except:
+            print("option 1 fail")
+        try:
+            print(self.foo(1,b=2,c=3))
+        except:
+            print("option 2 fail")
+        try:
+            print(self.foo(a=1,b=1,c=1))
+        except:
+            print("option 3 fail")
+
+    def foo(self,a, /, b, *, c):
+        sum_val = a + b + c
+        product_val = a * b * c
+        return product_val // sum_val
 
 if __name__ == "__main__":
-    SoHowBigIsX().run()
+    arrrg().run()
