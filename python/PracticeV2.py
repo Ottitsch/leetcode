f9781860df99796ccbb1e84e7cd28c04a1f663f3
Ottitsch <ottitsch.work@gmail.com>
2025-07-02 21:18:05 +0200

    generate parenthesis

--- python/PracticeV2.py
+++ python/PracticeV2.py
@@ -156,3 +156,21 @@ def isValid(s: str) -> bool:
         elif len(stack) == 0 or d[stack.pop()] != i:  # 2
             return False
     return len(stack) == 0
+
+def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+
+        def dfs(openP, closeP, s):
+            if openP == closeP and openP + closeP == n * 2:
+                res.append(s)
+                return
+            
+            if openP < n:
+                dfs(openP + 1, closeP, s + "(")
+            
+            if closeP < openP:
+                dfs(openP, closeP + 1, s + ")")
+
+        dfs(0, 0, "")
+
+        return res
