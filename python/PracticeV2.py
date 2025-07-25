56ab22fbeaa0d5db852f601dd9cfe2f8f0081230
ottitsch <e12024717@student.tuwien.ac.at>
2025-07-25 12:00:00 +0200

    Decode Ways

--- python/PracticeV2.py
+++ python/PracticeV2.py
@@ -245,3 +245,23 @@ def array_rank_transform(arr):
             mp[dup[i]] = mp[dup[i - 1]]
     
     return [mp[x] for x in arr]
+
+def numDecodings(self, s: str) -> int:
+    if not s or s[0] == '0':
+        return 0
+
+    n = len(s)
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        one_digit = int(s[i - 1])
+        two_digits = int(s[i - 2:i])
+
+        if one_digit != 0:
+            dp[i] += dp[i - 1]
+        if 10 <= two_digits <= 26:
+            dp[i] += dp[i - 2]
+
+    return dp[n]
