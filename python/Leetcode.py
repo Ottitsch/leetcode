9a2e58a11dbb15a9d86dbdb7b74ae286412282bf
Ottitsch <ottitsch.work@gmail.com>
2025-09-14 17:23:49 +0200

    Divide Players Into Teams of Equal Skill

--- python/Leetcode.py
+++ python/Leetcode.py
@@ -559,3 +559,18 @@ def toGoatLatin(self, S: str) -> str:
             temp.append(word)
 			
         return ans.join(temp)
+
+def dividePlayers(self, skill):
+        skill.sort()
+        ans, sum_ = 0, 0
+        ptr1, ptr2 = 0, len(skill) - 1
+        for i in range(len(skill) // 2):
+            if i == 0:
+                sum_ = skill[ptr1] + skill[ptr2]
+                ans += (skill[ptr1] * skill[ptr2])
+            else:
+                if (skill[ptr1] + skill[ptr2]) != sum_: return -1
+                ans += (skill[ptr1] * skill[ptr2])
+            ptr1 += 1
+            ptr2 -= 1
+        return ans
