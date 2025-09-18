f520132c9482afa35aa8ae855a841781046bbff5
ottitsch <e12024717@student.tuwien.ac.at>
2025-09-18 22:30:00 +0200

    Goat Latin

--- python/PracticeV2.py
+++ python/PracticeV2.py
@@ -269,3 +269,19 @@ def numDecodings(self, s: str) -> int:
 def grayCode(self, n: int) -> List[int]:
     result = [i ^ (i // 2) for i in range(pow(2, n))]
     return result
+
+def toGoatLatin(self, S: str) -> str:
+        temp = []
+        ans = " "
+        i = 1
+        vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+        for word in S.split(" "):
+            if word[0] in vowel:
+                word = word + "ma"
+            else:
+                word = word[1:] + word[0] + "ma"
+            word = word + "a"*i
+            i = i + 1
+            temp.append(word)
+			
+        return ans.join(temp)
